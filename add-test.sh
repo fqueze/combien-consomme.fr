31f4b324f8018b5f12ac622f0813ca4621367ce7
Florian Quèze <florian@queze.net>
2024-12-28 12:31:45 +0100

    Add add-test.sh script to quickly commit a new test.

---
+++ add-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Example usage: ./add-test.sh tests/pompe-de-relevage-de-condensats-sanicondens-eco.md
+
+fileprefix=`basename "$1"|sed 's/\.md$//'`
+title=`grep "title:" "$1"|head -n 1|sed 's/title: //'`
+
+git add $1 "{images,profiles}/$fileprefix*" && git commit -m "\"Ajout du test d'$title.\""
