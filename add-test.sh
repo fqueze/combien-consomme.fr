a3fe652f524ecea38a21186cd053114cb0172f89
Florian Quèze <florian@queze.net>
2025-04-19 17:42:11 +0200

    Remove useless double quotes in commit messages generated by add-test.sh.

--- add-test.sh
+++ add-test.sh
@@ -5,4 +5,4 @@
 fileprefix=`basename "$1"|sed 's/\.md$//'`
 title=`grep "title:" "$1"|head -n 1|sed 's/title: //'`
 
-git add $1 {images,profiles}/"$fileprefix"* && git commit -m "\"Ajout du test d'$title.\""
+git add $1 {images,profiles}/"$fileprefix"* && git commit -m "Ajout du test d'$title."
